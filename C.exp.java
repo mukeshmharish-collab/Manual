6ff9fd5822d36d11e1d9cf5b87cf7623248f5ca1
Mukesh M <mukeshmharish@gmail.com>
2026-07-25 09:59:59 +0530

    Update C.exp.java

--- C.exp.java
+++ C.exp.java
@@ -1,91 +1,40 @@
-import java.util.Scanner;
+class DataContainer<T> {
+    private T data;
 
-class Board {
-    private char[][] grid = {
-        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
-    };
-
-    public void display() {
-        for (int i = 0; i < 8; i++) {
-            System.out.print((8 - i) + "  ");
-            for (int j = 0; j < 8; j++) {
-                System.out.print(grid[i][j] + "  ");
-            }
-            System.out.println();
-        }
-        System.out.println("   a  b  c  d  e  f  g  h");
+    public void setData(T data) {
+        this.data = data;
     }
 
-    public boolean movePiece(String from, String to, boolean whiteTurn) {
-        if (from.length() != 2 || to.length() != 2) return false;
-
-        int fromCol = from.charAt(0) - 'a';
-        int fromRow = 8 - (from.charAt(1) - '0');
-        int toCol = to.charAt(0) - 'a';
-        int toRow = 8 - (to.charAt(1) - '0');
-
-        if (fromRow < 0 || fromRow > 7 || fromCol < 0 || fromCol > 7 ||
-            toRow < 0 || toRow > 7 || toCol < 0 || toCol > 7) {
-            return false;
-        }
-
-        char piece = grid[fromRow][fromCol];
-        if (piece == '.') return false;
-
-        grid[toRow][toCol] = piece;
-        grid[fromRow][fromCol] = '.';
-
-        System.out.println(getPieceName(piece) + " moved from " + from + " to " + to + ".");
-        return true;
-    }
-
-    private String getPieceName(char piece) {
-        switch (Character.toLowerCase(piece)) {
-            case 'p': return "Pawn";
-            case 'r': return "Rook";
-            case 'n': return "Knight";
-            case 'b': return "Bishop";
-            case 'q': return "Queen";
-            case 'k': return "King";
-            default: return "Piece";
-        }
+    public T getData() {
+        return data;
     }
 }
 
-public class Main {
+public class GenericContainer {
+
+    public static <T> void display(T value) {
+        System.out.println("Value : " + value);
+    }
+
+    public static <T extends Number> double square(T number) {
+        return number.doubleValue() * number.doubleValue();
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Board board = new Board();
-        boolean whiteTurn = true;
 
-        System.out.println("=== Chess Game (Console Version) ===");
-        System.out.println("Initial Board Setup:");
-        board.display();
+        DataContainer<Integer> intData = new DataContainer<>();
+        intData.setData(25);
 
-        while (true) {
-            System.out.println("\n" + (whiteTurn ? "White's" : "Black's") + " turn.");
-            System.out.print("Enter move (e.g., e2 e4) or 'exit': ");
-            
-            String from = sc.next();
-            if (from.equalsIgnoreCase("exit")) break;
+        DataContainer<String> strData = new DataContainer<>();
+        strData.setData("Mukesh");
 
-            String to = sc.next();
-            System.out.println();
-            
-            boolean moved = board.movePiece(from, to, whiteTurn);
-            System.out.println();
-            board.display();
+        System.out.println("Integer Data : " + intData.getData());
+        System.out.println("String Data : " + strData.getData());
 
-            if (moved) whiteTurn = !whiteTurn;
-        }
+        display(intData.getData());
+        display(strData.getData());
 
-        sc.close();
+        System.out.println("Square of Integer : " + square(5));
+        System.out.println("Square of Double : " + square(7.5));
     }
 }
